df7a1479819ad822d81b207de370a2281e41e13f
webDevSaurav <webdevelopersaurav@gmail.com>
2023-07-03 20:56:52 +0530

    adding the navigator

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/models/category.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  //here state is not updated however Navigation is used
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where(
+          (meal) => meal.categories.contains(category.id),
+        )
+        .toList();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    );
+    //Navigator.of(context).push(route);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +44,12 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           //map can be used
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
