7102abe6f87622bfe7bdd03c50bd4d54ae3ce8f9
Saurav Ganguly <hisauravganguly@gmail.com>
2023-07-12 21:27:28 +0530

    adding riverpod and inital setup

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,6 +1,9 @@
 //tab navigation requires own screen
 import 'package:flutter/material.dart';
-import 'package:meals/data/dummy_data.dart';
+//provider
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:meals/providers/meals_provider.dart';
+
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
@@ -14,14 +17,14 @@ const kInitialFilter = {
   Filter.vegan: false,
 };
 
-class TabsScreen extends StatefulWidget {
+class TabsScreen extends ConsumerStatefulWidget {
   const TabsScreen({super.key});
 
   @override
-  State<TabsScreen> createState() => _TabsScreenState();
+  ConsumerState<TabsScreen> createState() => _TabsScreenState();
 }
 
-class _TabsScreenState extends State<TabsScreen> {
+class _TabsScreenState extends ConsumerState<TabsScreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
   Map<Filter, bool> _selectedFilters = kInitialFilter;
@@ -72,7 +75,12 @@ class _TabsScreenState extends State<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final availableMeals = dummyMeals.where((meal) {
+    // ref -> listeners to providers
+    // ref.read() -> get values
+    // ref.watch() -> build is run again when the data changes
+    final meals = ref
+        .watch(mealsProvider); // data in meals provider changes then it is run
+    final availableMeals = meals.where((meal) {
       if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
         return false;
       }
